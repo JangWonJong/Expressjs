b041f360c5866a44070721c5ad470d94efe9734a
JangWonJong <jwj9922@naver.com>
2022-04-12 17:58:16 +0900

    수업끝커밋

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,7 +1,7 @@
 const { signup, userlist, profile } = require('../controllers/user.controller')
 module.exports = x => {x.app.post(`${x.url}/sign-up`, signup)
                        x.app.get(`${x.url}/list`, userlist)
-                       x.app.get(`${x/url}/profile/:id`, profile)}
+                       x.app.get(`${x.url}/profile/:id`, profile)}
 
 
 /**
