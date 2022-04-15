e2518a54f1c01701f07bd608ad6bc4194d42efa8
JangWonJong <jwj9922@naver.com>
2022-04-15 18:40:17 +0900

    최종커밋

--- app/controllers/todo.controller.js
+++ app/controllers/todo.controller.js
@@ -1,7 +1,7 @@
 const db = require('../models/index')
 const TodoSchema = db.todo
 
-exports.todoo = (req, res) =>{
+exports.todo = (req, res) =>{
     console.log(' ### 진행 4: 노드서버에 진입함 ' + JSON.stringify(req.body))
     new TodoSchema(req.body
         ).save(()=>{
@@ -12,8 +12,8 @@ exports.todoo = (req, res) =>{
 
 exports.todolist = (req, res) =>{
     console.log(` ### todoController acess ### `)
-    UserSchema.find()
-    .exec((err, users)=>{
+    TodoSchema.find()
+    .exec((err, todos)=>{
         if (err) return res.status(400).send(err)
         res.status(200).json({ success: true, todos})
     })
--- app/routes/todo.route.js
+++ app/routes/todo.route.js
@@ -1,3 +1,3 @@
-const { todolist, todoo } = require('../controllers/todo.controller');
-module.exports = x => {x.app.post(`${x.url}/list`, todolist)
-                       x.app.post(`${x.url}/do`, todoo) }
+const { todolist, todo } = require('../controllers/todo.controller');
+module.exports = x => {x.app.post(`${x.url}/todolist`, todolist)
+                       x.app.post(`${x.url}/do`, todo) }
